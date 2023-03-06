7abfe982384a6afe9481cb5e49811316b818bd07
Karel Braeckman <karel.braeckman@limecraft.com>
2023-03-06 10:17:50 +0100

    bump version + create dist version

--- tributejs.d.ts
+++ tributejs.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for TributeJS v5.1.3
+// Type definitions for TributeJS
 // Project: https://github.com/zurb/tribute
 // Definitions by: Jordan Humphreys <https://github.com/mrsweaters/>
 
